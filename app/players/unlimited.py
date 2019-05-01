01110a4bb5c158b331fc3ad4b269ffec937ca1d6
Diana <pdsanaa@gmail.com>
2019-05-01 20:42:11 +0300

    the main conception of the unlimited algorithm

---
+++ app/players/unlimited.py
@@ -0,0 +1,39 @@
+grid = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+
+def compute_best_move(grid, depth=2):
+    if depth == 0:
+        # do estimation
+
+        return None, mark
+
+    else:
+        possible_moves = compute_possible_moves(grid)
+
+        best_move, best_mark = None, -1e9
+
+        for current_move in possible_moves:
+            current_grid = copy(grid)
+            grid.move(current_move)
+
+            _, current_mark = compute_best_move(grid, depth=depth-1)
+            if current_mark > best_mark:
+                best_mark = current_mark
+                best_move = current_move
+
+        return best_move, best_mark
