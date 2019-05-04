003c177bd27b526b3d0a10606347ff1f66201e1a
Diana <pdsanaa@gmail.com>
2019-05-04 17:33:46 +0300

    Fix horizontally

--- app/players/unlimited.py
+++ app/players/unlimited.py
@@ -39,52 +39,17 @@ def compute_best_move(grid, depth=2):
         return best_move, best_mark
 
 
-def score_estimation(number):
-    if number == 0:
-        return 0
-    if number == 1:
-        return 1
-    if number == 2:
-        return 10
-    if number == 3:
-        return 100
-    if number == 4:
-        return 1000
-    if number == 5:
-        return 10000
-
-
 def estimation(grid, player):
     grid_score = 0
+
     #horizontally
-    for i in range(15):
-        for j in range(15-4):
+    for i in range(len(grid)):
+        for j in range(len(grid[0]) - 4):
+            cells = grid[i][j:j+5]
 
-            index = j
-            finish_index = j + 5
-            max_length_score = 0
-            current_score = 0
-
-            while(index < finish_index):
-                if grid[i][index] == player:
-                    current_score += 1
-                    print(f"{i} {j}")
-                    print(f"{i} {index}")
-                elif grid[i][index] == player * -1:
-                    max_length_score = 0
-                    break
-                else:
-                    if current_score > max_length_score:
-                        max_length_score = current_score
-                        current_score = 0
-                if current_score > max_length_score:
-                    max_length_score = current_score
-                
-                index += 1
-            
-            grid_score += score_estimation(max_length_score)
-            if score_estimation(max_length_score) > 0:
-                print(score_estimation(max_length_score))
+            if not cells.count(-player):
+                if cells.count(player):
+                    grid_score += 10 ** (cells.count(player) - 1)
 
     return grid_score
 
