a6a72fdaf978ef4fa95da05c54621987764a7cd2
andreyTheGreatest <manokhin.kpi@gmail.com>
2020-09-10 09:19:46 +0200

    sql dump with user data only added

--- dump-editor.sql
+++ dump-editor.sql
@@ -56,16 +56,6 @@ CREATE TABLE `user_file_created` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user_file_created`
---
-
-LOCK TABLES `user_file_created` WRITE;
-/*!40000 ALTER TABLE `user_file_created` DISABLE KEYS */;
-INSERT INTO `user_file_created` VALUES (1,'C:\\Users\\user\\IdeaProjects\\TextVerarbeiter\\username.txt'),(1,'C:\\Users\\user\\IdeaProjects\\TextVerarbeiter\\username1.txt');
-/*!40000 ALTER TABLE `user_file_created` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `user_memento`
 --
 
@@ -80,24 +70,5 @@ CREATE TABLE `user_memento` (
   CONSTRAINT `user_memento_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user_memento`
---
-
-LOCK TABLES `user_memento` WRITE;
-/*!40000 ALTER TABLE `user_memento` DISABLE KEYS */;
-INSERT INTO `user_memento` VALUES (2,'C:\\Users\\user\\IdeaProjects\\TextVerarbeiter\\src\\editor\\database\\User.java','file'),(2,'C:\\Users\\user\\IdeaProjects\\TextVerarbeiter\\src\\editor\\database\\UserDataAccessor.java','file'),(2,'C:\\Users\\user\\IdeaProjects\\TextVerarbeiter\\src\\editor\\database','dir'),(1,'C:\\Users\\user\\IdeaProjects\\TextVerarbeiter\\src\\editor\\controllers','dir');
-/*!40000 ALTER TABLE `user_memento` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2020-09-10  9:13:47
