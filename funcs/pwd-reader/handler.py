0e977400b3525a8f713ab0d6e5c776ae4d3d06ea
Denys Chernyshov <den.blackshov@gmail.com>
2022-07-11 17:17:24 +0200

    add files

--- funcs/pwd-reader/handler.py
+++ funcs/pwd-reader/handler.py
@@ -1,7 +1,27 @@
-def handle(req):
-    """handle a request to the function
-    Args:
-        req (str): request body
-    """
+import mysql.connector
+from mysql.connector import Error
 
-    return req
+def handle(req):
+    connection = None 
+    try :
+        connection = mysql.connector.connect(user='root', password='1234',
+                                host='127.0.0.1',
+                                database='db')
+        return read_passwords_from_db(connection)
+    except Error as e:
+        error = "Error while connecting to MySQL:" + e.msg 
+        print(error)
+        raise e
+    finally:
+        if connection is not None and connection.is_connected():
+            connection.close()
+            print("MySQL connection is closed")
+
+
+def read_passwords_from_db(connection, data):
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM passwords")
+    return cursor.fetchall()
+
+
+print(handle(None))
