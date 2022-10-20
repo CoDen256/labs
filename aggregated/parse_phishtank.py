cc0356d70479d4b7c77254c58bad460b7aad5ada
Denys Chernyshov <denys.chernyshov@optimax-energy.de>
2022-10-20 16:59:31 +0200

    parsing phishtank

---
+++ aggregated/parse_phishtank.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import csv
+import sys
+import requests
+
+fields = ['Network','Domain Name', 'Updated Date', 'Creation Date', 'Registry Expiry Date', 'Registrar', 'Registrant Name',
+              'Registrant Organization', 'Registrant Country']
+
+def parse_phishtank(link_id):
+    print(f"requesting {link_id}")
+    data = {}
+    r = requests.get(f'http://phishtank.org/technical_details.php?phish_id={link_id}')
+    response = BeautifulSoup(r.text, 'html.parser')
+    text = response.find_all('pre')
+    for field in fields:
+        data[field] = None
+    data['Network'] = text[0].text.split(' ')[0]
+    for value in text[1].text.split('\r\n'):
+        v = value.split(': ')
+        if v[0].strip() in fields:
+            data[f'{v[0]}'] = v[1]
+    print(data)
+    print("\n"*2)
+    return data
+
+
+if __name__ == '__main__':
+    with open("C:\\dev\\dataset-urls\\aggregated\\phishtank.csv", 'r') as r:
+        with open("parsed_phishtank.csv", 'w', newline="") as f:
+            reader = csv.reader(r, delimiter=",")
+            writer = csv.writer(f)
+            writer.writerow(fields)
+            for row in reader:
+                try:
+                    data = parse_phishtank(row[0])
+                    # writer.writerow(data.keys())
+                    #Network,Domain Name,Updated Date,Creation Date,Registry Expiry Date,Registrar,Registrant Name,Registrant Organization,Registrant Country
+                    writer.writerow(data.values())
+                except Exception as e:
+                    print(e)
+                    pass
