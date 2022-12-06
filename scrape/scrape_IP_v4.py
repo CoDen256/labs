77ef6f134f65fe10dccb209ec98ada5327465aea
Denys Chernyshov <denys.chernyshov@optimax-energy.de>
2022-12-06 10:31:56 +0100

    add more ips

--- scrape/scrape_IP_v4.py
+++ scrape/scrape_IP_v4.py
@@ -4,6 +4,6 @@ from scrape_IP import *
 
 load_scrape_save("datasets/group/IPv4.csv", "datasets/parsed/IPv4_parsed.csv",
                  lambda ip: scrape_ip(ip, IndicatorTypes.IPv4),
-                 start=0,
-                 count=300,
+                 start=300,
+                 count=100,
                  batch_size=50)
--- scrape/scrape_IP_v6.py
+++ scrape/scrape_IP_v6.py
@@ -5,6 +5,6 @@ from scrape_IP import *
 
 load_scrape_save("datasets/group/IPv6.csv", "datasets/parsed/IPv6_parsed.csv",
                  lambda ip: scrape_ip(ip, IndicatorTypes.IPv6),
-                 start=0,
-                 count=300,
-                 batch_size=50)
+                 start=300,
+                 count=100,
+                 batch_size=10)
