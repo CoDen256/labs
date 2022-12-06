7921dc1fe7c8ec451227ab01fad1699b2d78bc14
Denys Chernyshov <denys.chernyshov@optimax-energy.de>
2022-12-06 13:25:19 +0100

    Finish file unifying

--- unify/unify.py
+++ unify/unify.py
@@ -3,18 +3,31 @@ from typing import List
 
 
 def unify_hostnames_domains_ips(hostnames: List[Hostname], domains: List[Domain], ips: List[IP]):
-    total = []
+    total = set()
     primary_domains = list(map(lambda h: h.domain, hostnames))
     primary_hostnames = list(map(lambda h: h.hostname, hostnames))
 
     result_ip = [ip for ip in ips if ip.hostname not in primary_hostnames]
     result_domains = [domain for domain in domains if domain.domain not in primary_domains]
 
-    total += list(set(result_ip))
-    total += list(set(hostnames))
-    total += list(set(result_domains))
+    total.update(result_ip)
+    total.update(hostnames)
+    total.update(result_domains)
     return total
 
 
-def unify_md5_sha1_sha256(md5, sha1, sha256):
-    pass
+def unify_md5_sha1_sha256(md5: List[FileHashMD5], sha1: List[FileHashSHA1], sha256: List[FileHashSHA256]):
+    total = set()
+
+    primary_ids = list(map(lambda sha256_hash: sha256_hash.id, sha256))
+
+    result_md5 = list(filter(lambda file_hash: file_hash.id not in primary_ids, md5))
+    secondary_ids = list(map(lambda md5_hash: md5_hash.id, result_md5))
+
+    result_sha1 = list(filter(lambda file_hash: file_hash.id not in primary_ids and file_hash.id not in secondary_ids,
+                              sha1))
+
+    total.update(sha256)
+    total.update(result_sha1)
+    total.update(result_md5)
+    return total
--- unify/unify_test.py
+++ unify/unify_test.py
@@ -79,11 +79,11 @@ class Test(TestCase):
 
         result = unify_hostnames_domains_ips(hostnames, domains, ips)
 
-        self.assertCountEqual([
+        self.assertCountEqual({
             Hostname("unique_hostname", "unique_hostname", 0),
             Hostname("h", "d1", 0),
             Domain("unique_domain", 20)
-        ], result)
+        }, result)
 
     def test_unify_hostnames_ips(self):
         hostnames = [Hostname("unique_hostname", "unique_hostname", 0), Hostname("h", "d1", 0)]
@@ -92,14 +92,15 @@ class Test(TestCase):
 
         result = unify_hostnames_domains_ips(hostnames, domains, ips)
 
-        self.assertCountEqual([
+        self.assertCountEqual({
             Hostname("unique_hostname", "unique_hostname", 0),
             Hostname("h", "d1", 0),
             IP("unique_ip", "unique_ip_hostname", 0)
-        ], result)
+        }, result)
 
     def test_unify_hostnames_domains_ips(self):
         hostnames = [Hostname("unique_hostname", "unique_hostname_domain", 0),
+                     Hostname("unique_hostname", "unique_hostname_domain", 0),
                      Hostname("h0", "d0", 0),
                      Hostname("h1", "d1", 1),
                      Hostname("h2", "d2", 1)
@@ -108,28 +109,31 @@ class Test(TestCase):
             Domain("d0", 10),
             Domain("d1", 10),
             Domain("unique_domain_0", 20),
+            Domain("unique_domain_0", 20),
         ]
 
         ips = [
             IP("ip", "h0", 0),
             IP("ip", "h2", 0),
             IP("unique_ip_0", "unique_ip_hostname_0", 0),
+            IP("unique_ip_0", "unique_ip_hostname_0", 0),
         ]
+
         result = unify_hostnames_domains_ips(hostnames, domains, ips)
 
-        self.assertCountEqual([
+        self.assertCountEqual({
             Hostname("unique_hostname", "unique_hostname_domain", 0),
             Hostname("h0", "d0", 0),
             Hostname("h1", "d1", 1),
             Hostname("h2", "d2", 1),
             Domain("unique_domain_0", 20),
             IP("unique_ip_0", "unique_ip_hostname_0", 0),
-        ], result)
+        }, result)
 
     def test_unify_domains_ips(self):
         hostnames = []
         domains = [Domain("0", 10), Domain("1", 20)]
-        ips = [IP("0", "0", 0), IP("1", "1", 0),]
+        ips = [IP("0", "0", 0), IP("1", "1", 0), ]
 
         result = unify_hostnames_domains_ips(hostnames, domains, ips)
 
@@ -137,8 +141,215 @@ class Test(TestCase):
             domains + ips, result
         )
 
-    def test_files(self):
-        md5 = []
+    def test_unify_md5(self):
+        # ARRANGE
+        md5 = [FileHashMD5("m", 0, 0), FileHashMD5("m", 1, 0)]
         sha1 = []
         sha256 = []
+
+        # ACT
         result = unify_md5_sha1_sha256(md5, sha1, sha256)
+
+        # ASSERT
+        self.assertCountEqual(
+            md5 + sha1 + sha256
+            , result
+        )
+
+    def test_unify_md5_same(self):
+        # ARRANGE
+        md5 = [FileHashMD5("m", 0, 0), FileHashMD5("m", 0, 0)]
+        sha1 = []
+        sha256 = []
+
+        # ACT
+        result = unify_md5_sha1_sha256(md5, sha1, sha256)
+
+        # ASSERT
+        self.assertCountEqual(
+            md5[:1] + sha1 + sha256
+            , result
+        )
+
+    def test_unify_sha1(self):
+        # ARRANGE
+        md5 = []
+        sha1 = [FileHashSHA1("1", 0, 0), FileHashSHA1("1", 1, 0)]
+        sha256 = []
+
+        # ACT
+        result = unify_md5_sha1_sha256(md5, sha1, sha256)
+
+        # ASSERT
+        self.assertCountEqual(
+            md5 + sha1 + sha256
+            , result
+        )
+
+    def test_unify_sha1_same(self):
+        # ARRANGE
+        md5 = []
+        sha1 = [FileHashSHA1("1", 0, 0), FileHashSHA1("1", 0, 0)]
+        sha256 = []
+
+        # ACT
+        result = unify_md5_sha1_sha256(md5, sha1, sha256)
+
+        # ASSERT
+        self.assertCountEqual(
+            md5 + sha1[:1] + sha256
+            , result
+        )
+
+    def test_unify_sha256(self):
+        # ARRANGE
+        md5 = []
+        sha1 = []
+        sha256 = [FileHashSHA256("2", 0, 0), FileHashSHA256("2", 1, 0)]
+
+        # ACT
+        result = unify_md5_sha1_sha256(md5, sha1, sha256)
+
+        # ASSERT
+        self.assertCountEqual(
+            md5 + sha1 + sha256
+            , result
+        )
+
+    def test_unify_sha256_same(self):
+        # ARRANGE
+        md5 = []
+        sha1 = []
+        sha256 = [FileHashSHA256("2", 0, 0), FileHashSHA256("2", 0, 0)]
+
+        # ACT
+        result = unify_md5_sha1_sha256(md5, sha1, sha256)
+
+        # ASSERT
+        self.assertCountEqual(
+            md5 + sha1 + sha256[:1]
+            , result
+        )
+
+    def test_unify_md5_sha256(self):
+        # ARRANGE
+        md5 = [FileHashMD5("m", -1, 0), FileHashMD5("m", 1, 0)]
+        sha1 = []
+        sha256 = [FileHashSHA256("2", -2, 0), FileHashSHA256("2", 1, 0)]
+
+        # ACT
+        result = unify_md5_sha1_sha256(md5, sha1, sha256)
+
+        # ASSERT
+        self.assertCountEqual(
+            {
+                FileHashMD5("m", -1, 0),
+                FileHashSHA256("2", -2, 0),
+                FileHashSHA256("2", 1, 0),
+            }
+            , result
+        )
+
+    def test_unify_md5_sha1(self):
+        # ARRANGE
+        md5 = [FileHashMD5("m", 0, 0), FileHashMD5("m", -1, 0)]
+        sha1 = [FileHashSHA1("1", 0, 0), FileHashSHA1("1", 1, 0)]
+        sha256 = []
+
+        # ACT
+        result = unify_md5_sha1_sha256(md5, sha1, sha256)
+
+        # ASSERT
+        self.assertCountEqual(
+            {
+                FileHashSHA1("1", 1, 0),
+                FileHashMD5("m", -1, 0),
+                FileHashMD5("m", 0, 0),
+
+            }
+            , result
+        )
+
+    def test_unify_sha1_sha256(self):
+        # ARRANGE
+        md5 = []
+        sha1 = [FileHashSHA1("1", 0, 0), FileHashSHA1("1", -1, 0)]
+        sha256 = [FileHashSHA256("2", 0, 0), FileHashSHA256("2", 1, 0)]
+
+        # ACT
+        result = unify_md5_sha1_sha256(md5, sha1, sha256)
+
+        # ASSERT
+        self.assertCountEqual(
+            {
+                FileHashSHA256("2", 0, 0),
+                FileHashSHA256("2", 1, 0),
+                FileHashSHA1("1", -1, 0)
+            }
+            , result
+        )
+
+    def test_unify_md5_sha1_sha256(self):
+        # ARRANGE
+        sha256 = [
+            FileHashSHA256("2", -1, 0),
+            FileHashSHA256("2", -1, 0),
+
+            FileHashSHA256("2", 0, 0), # sha256 + md5
+            FileHashSHA256("2", 1, 0), # sha256 + md5 + sha1
+            FileHashSHA256("2", 2, 0), # sha256 + sha1
+
+        ]
+        md5 = [
+            FileHashMD5("m", -2, 0),
+            FileHashMD5("m", -2, 0),
+
+            FileHashMD5("m", 0, 0), # sha256 + md5
+            FileHashMD5("m", 1, 0), # sha256 + md5 + sha1
+
+            FileHashMD5("m", 3, 0)  # md5 + sha1
+        ]
+        sha1 = [
+            FileHashSHA1("1", -3, 0),
+            FileHashSHA1("1", -3, 0),
+
+            FileHashSHA1("1", 1, 0),  # sha256 + md5 + sha1
+            FileHashSHA1("1", 2, 0),  # sha256 + sha1
+
+            FileHashSHA1("1", 3, 0)   # md5 + sha1
+        ]
+
+        # ACT
+        result = unify_md5_sha1_sha256(md5, sha1, sha256)
+
+        # ASSERT
+        self.assertCountEqual(
+            {
+                FileHashSHA256("2", -1, 0),
+                FileHashMD5("m", -2, 0),
+                FileHashSHA1("1", -3, 0),
+
+                FileHashSHA256("2", 0, 0),
+                FileHashSHA256("2", 1, 0),
+                FileHashSHA256("2", 2, 0),
+
+                FileHashMD5("m", 3, 0)
+
+            }
+            , result
+        )
+
+    def test_unify_md5_sha256_sha1_unique(self):
+        # ARRANGE
+        md5 = [FileHashMD5("m", 0, 0), FileHashMD5("m", 1, 0)]
+        sha1 = [FileHashSHA1("1", 2, 0), FileHashSHA1("1", 3, 0)]
+        sha256 = [FileHashSHA256("2", 4, 0), FileHashSHA256("2", 5, 0)]
+
+        # ACT
+        result = unify_md5_sha1_sha256(md5, sha1, sha256)
+
+        # ASSERT
+        self.assertCountEqual(
+            md5 + sha1 + sha256
+            , result
+        )
