b40fd1fe3f261beb759fafbc487247a8ece690f2
Denys Chernyshov <denys.chernyshov@optimax-energy.de>
2022-12-16 14:29:17 +0100

    fix verification report

--- report_utils.py
+++ report_utils.py
@@ -4,22 +4,37 @@ from typing import List
 from model import SystemSnapshot, InitializationReport, VerificationReport, SnapshotDiff, FileSnapshot, DirSnapshot
 
 
-def create_init_report(monitored_path: str, verification_path: str, time_millis: int,
-                       snapshot: SystemSnapshot) -> InitializationReport:
+def count_dirs_files(snapshot: SystemSnapshot) -> tuple[int, int]:
     snapshots = sorted(map(lambda s: str(type(s)), snapshot.snapshots))
     grouped_count_by_type = dict((key, len(list(group))) for key, group in groupby(snapshots))
+    return grouped_count_by_type[str(DirSnapshot)], grouped_count_by_type[str(FileSnapshot)]
+
+
+def create_init_report(monitored_path: str, verification_path: str, time_millis: int,
+                       snapshot: SystemSnapshot) -> InitializationReport:
+    dirs, files = count_dirs_files(snapshot)
     return InitializationReport(
         monitored_path,
         verification_path,
-        grouped_count_by_type[str(DirSnapshot)],
-        grouped_count_by_type[str(FileSnapshot)],
+        dirs,
+        files,
         time_millis
     )
 
 
-def create_verification_report(monitored_path: str, verification_path: str, time_millis: int, snapshot: SystemSnapshot,
+def create_verification_report(monitored_path: str, verification_path: str, time_millis: int,
+                               snapshot: SystemSnapshot,
                                diffs: List[SnapshotDiff]) -> VerificationReport:
-    pass
+    dirs, files = count_dirs_files(snapshot)
+    return VerificationReport(
+        monitored_path,
+        verification_path,
+        dirs,
+        files,
+        time_millis,
+        len(diffs),
+        diffs
+    )
 
 
 def write_init_report(report: InitializationReport, report_file: str):
--- test_report_utils.py
+++ test_report_utils.py
@@ -1,8 +1,8 @@
 import datetime
 from unittest import TestCase
 
-from model import SystemSnapshot, FileSnapshot, DirSnapshot
-from report_utils import create_init_report
+from model import SystemSnapshot, FileSnapshot, DirSnapshot, SnapshotDiff
+from report_utils import create_init_report, create_verification_report
 
 
 class Test(TestCase):
@@ -24,3 +24,41 @@ class Test(TestCase):
         self.assertEqual(report.execution_time, 10)
         self.assertEqual(report.files_parsed, 5)
         self.assertEqual(report.directories_parsed, 3)
+
+    def test_create_verification_report(self):
+        snap = SystemSnapshot([
+            FileSnapshot("p", "u", "g", "a", datetime.datetime.now(), "m", 0),
+            FileSnapshot("p", "u", "g", "a", datetime.datetime.now(), "m", 0),
+            FileSnapshot("p", "u", "g", "a", datetime.datetime.now(), "m", 0),
+            FileSnapshot("p", "u", "g", "a", datetime.datetime.now(), "m", 0),
+            DirSnapshot("p", "u", "g", "a", datetime.datetime.now()),
+            DirSnapshot("p", "u", "g", "a", datetime.datetime.now()),
+            DirSnapshot("p", "u", "g", "a", datetime.datetime.now()),
+            DirSnapshot("p", "u", "g", "a", datetime.datetime.now()),
+            DirSnapshot("p", "u", "g", "a", datetime.datetime.now()),
+            DirSnapshot("p", "u", "g", "a", datetime.datetime.now()),
+        ], 'md5')
+
+        diffs = [
+            SnapshotDiff("modify", "path", "f", "size", 0, 1),
+            SnapshotDiff("modify", "path", "f", "size", 0, 1),
+            SnapshotDiff("add", "path", "f", "size", 0, 1),
+            SnapshotDiff("add", "path", "f", "object", 0, 1),
+
+            SnapshotDiff("add", "path", "f", "user", 0, 1),
+            SnapshotDiff("add", "path", "f", "group", 0, 1),
+            SnapshotDiff("delete", "path", "f", "size", 0, 1),
+            SnapshotDiff("add", "path", "f", "size", 0, 1),
+
+            SnapshotDiff("add", "path", "f", "size", 0, 1),
+            SnapshotDiff("add", "path", "f", "size", 0, 1),
+        ]
+        report = create_verification_report("monitored", "verification_file", 231, snap, diffs)
+
+        self.assertEqual(report.monitored_dir, "monitored")
+        self.assertEqual(report.verification_file, "verification_file")
+        self.assertEqual(report.execution_time, 231)
+        self.assertEqual(report.files_parsed, 4)
+        self.assertEqual(report.directories_parsed, 6)
+        self.assertEqual(report.warnings, 10)
+        self.assertCountEqual(report.diffs, diffs)
