c521808312dff4996c883466289f0b1922c8b9dc
Denys Chernyshov <den.blackshov@gmail.com>
2022-12-16 19:14:42 +0100

    improve tests

--- test_snapshot_generation.py
+++ test_snapshot_generation.py
@@ -1,10 +1,14 @@
 import os
 from unittest import TestCase, skip
 from snapshot_generation import create_dir_snapshot, create_file_snapshot, create_system_snapshot
-from os_utils import resolve
+from os_utils import resolve, get_modified
 from sys import platform
 from datetime import datetime as dt
 from model import FileSnapshot, DirSnapshot
+
+
+def m(path):
+    return get_modified(os.stat(path))
 
 
 class Test(TestCase):
@@ -22,7 +26,7 @@ class Test(TestCase):
         if platform == "linux":
             self.assertEqual("coden", snapshot.user)
             self.assertEqual("coden", snapshot.group)
-            self.assertEqual("777", snapshot.access_mode)
+            self.assertEqual("664", snapshot.access_mode)
         else:
             self.assertEqual("0", snapshot.user)
             self.assertEqual("0", snapshot.group)
@@ -38,7 +42,7 @@ class Test(TestCase):
         if platform == "linux":
             self.assertEqual("coden", snapshot.user)
             self.assertEqual("coden", snapshot.group)
-            self.assertEqual("777", snapshot.access_mode)
+            self.assertEqual("775", snapshot.access_mode)
         else:
             self.assertEqual("0", snapshot.user)
             self.assertEqual("0", snapshot.group)
@@ -48,46 +52,38 @@ class Test(TestCase):
     def test_create_system_snapshot_windows(self):
         target = resolve("./test/generate/full")
         result = create_system_snapshot(target, "md5")
-        user = "0"
-        group = "0"
-        expected = [
-            FileSnapshot(resolve("./test/generate/full/file-1.txt"), user, group, "666", dt(2022, 12, 16, 18, 47, 12),
-                         "4462256a6ee3a6461a935e4cf02b8798", 287),
-            FileSnapshot(resolve("./test/generate/full/file-2.txt"), user, group, "666", dt(2022, 12, 16, 18, 47, 15),
-                         "0cc175b9c0f1b6a831c399e269772661", 1),
-            FileSnapshot(resolve("./test/generate/full/sub/file-4.txt"), user, group, "666",
-                         dt(2022, 12, 16, 18, 47, 23), "8bc2398146fb71fcdc2863d05d29433f", 1904),
-            FileSnapshot(resolve("./test/generate/full/sub/sub-2/file-5.txt"), user, group, "666",
-                         dt(2022, 12, 16, 18, 47, 27), "1a6f8abe44f6b9c5a7c2435c43749dea", 17),
-
-            DirSnapshot(resolve("./test/generate/full/sub"), user, group, "777", dt(2022, 12, 16, 18, 10, 33)),
-            DirSnapshot(resolve("./test/generate/full/sub/sub-2"), user, group, "777", dt(2022, 12, 16, 18, 10, 19))
-        ]
+        expected = self.generate("0", "666", "0", "777")
 
         self.assertEqual("md5", result.hash_function)
 
         self.assertCountEqual(expected, result.snapshots)
 
     def test_create_system_snapshot_linux(self):
-        target = resolve("./test/generate/full-linux")
-        result = create_system_snapshot(target, "sha1")
-        user = "coden"
-        group = "coden"
-        mode = "777"
-        expected = [
-            FileSnapshot(resolve("./test/generate/full/file-1.txt"), user, group, mode, dt(2022, 12, 16, 18, 47, 12),
-                         "4462256a6ee3a6461a935e4cf02b8798", 287),
-            FileSnapshot(resolve("./test/generate/full/file-2.txt"), user, group, "%s" % mode,
-                         dt(2022, 12, 16, 18, 47, 15), "0cc175b9c0f1b6a831c399e269772661", 1),
-            FileSnapshot(resolve("./test/generate/full/sub/file-4.txt"), user, group, mode,
-                         dt(2022, 12, 16, 18, 47, 23), "8bc2398146fb71fcdc2863d05d29433f", 1904),
-            FileSnapshot(resolve("./test/generate/full/sub/sub-2/file-5.txt"), user, group, mode,
-                         dt(2022, 12, 16, 18, 47, 27), "1a6f8abe44f6b9c5a7c2435c43749dea", 17),
-
-            DirSnapshot(resolve("./test/generate/full/sub"), user, group, "777", dt(2022, 12, 16, 18, 10, 33)),
-            DirSnapshot(resolve("./test/generate/full/sub/sub-2"), user, group, "777", dt(2022, 12, 16, 18, 10, 19))
-        ]
+        target = resolve("./test/generate/full")
+        result = create_system_snapshot(target, "md5")
+        expected = self.generate("coden", "664", "coden", "775")
 
         self.assertEqual("md5", result.hash_function)
-
         self.assertCountEqual(expected, result.snapshots)
+
+    def generate(self, group, mode, user, dir_mode):
+        expected = [
+            FileSnapshot(resolve("./test/generate/full/file-1.txt"), user, group, mode,
+                         m("./test/generate/full/file-1.txt"),
+                         "4462256a6ee3a6461a935e4cf02b8798", 287),
+
+            FileSnapshot(resolve("./test/generate/full/file-2.txt"), user, group, mode,
+                         m("./test/generate/full/file-2.txt"), "0cc175b9c0f1b6a831c399e269772661", 1),
+
+            FileSnapshot(resolve("./test/generate/full/sub/file-4.txt"), user, group, mode,
+                         m("./test/generate/full/sub/file-4.txt"), "8bc2398146fb71fcdc2863d05d29433f", 1904),
+
+            FileSnapshot(resolve("./test/generate/full/sub/sub-2/file-5.txt"), user, group, mode,
+                         m("./test/generate/full/sub/sub-2/file-5.txt"), "1a6f8abe44f6b9c5a7c2435c43749dea", 17),
+
+            DirSnapshot(resolve("./test/generate/full/sub"), user, group, dir_mode,
+                        m("./test/generate/full/sub")),
+            DirSnapshot(resolve("./test/generate/full/sub/sub-2"), user, group, dir_mode,
+                        m("./test/generate/full/sub/sub-2"))
+        ]
+        return expected
