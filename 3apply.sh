4824711f2a0484bfbfa348f07dd628238716b11a
Denys Chernyshov <denys.chernyshov@optimax-energy.de>
2022-12-28 22:57:09 +0100

    add crl

--- 3apply.sh
+++ 3apply.sh
@@ -5,3 +5,7 @@ sudo cp ca1/private/server.key.pem /etc/ssl/private
 sudo cp ca1/certs/server.cert.pem /etc/ssl/certs
 sudo mkdir /etc/apache2/ssl.cert
 sudo cp ca1/certs/ca1.cert-chain.pem /etc/apache2/ssl.crt/
+
+nano /etc/apache2/sites-enabled/default-ssl.conf
+
+sudo service apache2 restart
---
+++ 4crl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# CRL
+
+openssl ca -config ca1/openssl.cnf -gencrl -out ca1/crl/ca1.crl.pem
+openssl crl -in ca1/crl/ca1.crl.pem -noout -text
+
+
+# REVOKE
+
+
+openssl genrsa -out ca1/private/dragos.ilie@bth.se.cert.pem 2048
+openssl req -config ca1/openssl.cnf -new -sha256 -key ca1/private/dragos.ilie@bth.se.key.pem -out ca1/csr/dragos.ilie@bth.se.scr.pem
+openssl req -text -noout -verify -in ca1/csr/dragos.ilie@bth.se.scr.pem
+openssl ca -config ca1/openssl.cnf -extensions usr_cert -notext -md sha256 -in ca1/csr/dragos.ilie@bth.se.scr.pem -out ca1/certs/dragos.ilie@bth.se.cert.pem
+openssl x509 -noout -text -in ca1/certs/dragos.ilie@bth.se.cert.pem
+openssl verify -CAfile certs/root.cert.pem -untrusted ca1/certs/ca1.cert.pem ca1/certs/dragos.ilie@bth.se.cert.pem
+
+openssl ca -config ca1/openssl.cnf -revoke ca1/certs/dragos.ilie@bth.se.cert.pem
+cat ca1/index.txt
+openssl crl -in ca1/crl/ca1.crl.pem -noout -text 
