e2c55741bc74a407fd0874f971b47073e778e7ee
Denys Chernyshov <denys.chernyshov@optimax-energy.de>
2022-12-30 00:59:01 +0100

    add additional init scripts and fix linedings

---
+++ 0network.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo cp /home/student/kvpn/orig/installer-config.sa.yaml /etc/netplan/00-installer-config.yaml
--- 5vpn_PSK.sh
+++ 5vpn_PSK.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-sudo apt-get install strongswan
-sudo apt-get install charon-systemd
-sudo nano /etc/ipsec.conf
-sudo nano /etc/ipsec.secrets
+# server A -> adapter 1 : .60 (#2) + adapter 2 : .70 (#3)
+# server B -> adapter 1 : .80 (#4) + adapter 2 : .70 (#3)
+
+# server A: .60.100  + .70.5
+# client A: .60.111  
+
+# server B: .80.100  + .70.6
+# client B: .80.111
+read T
+
+sudo apt-get update
+sudo apt-get install -y strongswan
+sudo apt-get install -y charon-systemd
+
+sudo cp /home/student/kvpn/vpn_psk/ipsec.conf.$T /etc/ipsec.conf
+sudo cp /home/student/kvpn/vpn_psk/ipsec.secrets.$T /etc/ipsec.secrets
+
+#sudo nano /etc/ipsec.conf
+#sudo nano /etc/ipsec.secrets
 
 #sudo systemctl enable strongswan-starter
 #sudo systemctl start strongswan-starter
