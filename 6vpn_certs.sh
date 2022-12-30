3d66ec2f90a86bc9144391841437cff11079935a
Denys Chernyshov <denys.chernyshov@optimax-energy.de>
2022-12-30 13:21:17 +0100

    fix line endings, fix 6_cert vpn

--- 6vpn_certs.sh
+++ 6vpn_certs.sh
@@ -23,42 +23,57 @@ openssl verify -CAfile certs/root.cert.pem -untrusted ca1/certs/ca1.cert.pem ca1
 
 
 ### Copy to strongswan
-# Copy CA and reread Server A and Server B
+# Copy CA and reread Server B and Server A
 
-# Server A
+
+## Server B CAs, Cert, Private Key, ipsec.conf + ipsec.secrets
+# SCP CAs + move to cacerts
+scp ca1/certs/ca1.cert.pem student@192.168.70.6:/tmp
+scp certs/root.cert.pem student@192.168.70.6:/tmp
+ssh -t student@192.168.70.6 "sudo mv /tmp/*.pem /etc/ipsec.d/cacerts/"
+
+# SCP key + move to private
+scp ca1/private/192.168.70.6.key.pem student@192.168.70.6:/tmp
+ssh -t student@192.168.70.6 "sudo mv /tmp/*.pem /etc/ipsec.d/private/"
+
+# SCP key + move to private
+scp ca1/certs/192.168.70.6.cert.pem student@192.168.70.6:/tmp
+ssh -t student@192.168.70.6 "sudo mv /tmp/*.pem /etc/ipsec.d/certs/"
+
+ssh -t student@192.168.70.6 "sudo ipsec rereadcacerts; sudo ipsec listcacerts"
+
+# SCP conf + move to /etc/
+scp /home/student/kvpn/vpn_cert/ipsec.conf.b student@192.168.70.6:/tmp/ipsec.conf
+ssh -t student@192.168.70.6 "sudo mv /tmp/ipsec.conf /etc/ipsec.conf"
+
+# SCP secrets + move to /etc/
+scp /home/student/kvpn/vpn_cert/ipsec.secrets.b student@192.168.70.6:/tmp/ipsec.secrets
+ssh -t student@192.168.70.6 "sudo mv /tmp/ipsec.secrets /etc/ipsec.secrets"
+
+# Server B: sudo ipsec restart && sudo ipsec statusall
+# ssh -t student@192.168.70.6 "sudo ipsec restart" &
+
+## Server A CAs, Cert, Private Key
 sudo cp ca1/certs/ca1.cert.pem /etc/ipsec.d/cacerts/
 sudo cp certs/root.cert.pem /etc/ipsec.d/cacerts/
+
+sudo cp ca1/private/192.168.70.5.key.pem /etc/ipsec.d/private/
+sudo cp ca1/certs/192.168.70.5.cert.pem /etc/ipsec.d/certs/
+
 sudo ipsec rereadcacerts
 sudo ipsec listcacerts
 
-
-# Server B
-scp ca1/certs/ca1.cert.pem student@192.168.70.6:/home/student/ca1.cert.pem 
-scp certs/root.cert.pem student@192.168.70.6:/home/student/root.cert.pem
-
-scp ca1/private/192.168.70.6.key.pem student@192.168.70.6:/home/student/192.168.70.6.key.pem
-scp ca1/certs/192.168.70.6.cert.pem student@192.168.70.6:/home/student/192.168.70.6.cert.pem
-
-ssh student@192.168.70.6
-mkdir private 
-mv *.pem private
-mv ipsec.* private
-cd private
-ls private
-
-sudo cp ca1.cert.pem /etc/ipsec.d/cacerts/
-sudo cp root.cert.pem /etc/ipsec.d/cacerts/
-sudo ipsec rereadcacerts
-sudo ipsec listcacerts
-
-exit
-# where to put certs ?
+sudo cp /home/student/kvpn/vpn_cert/ipsec.conf.a /etc/ipsec.conf
+sudo cp /home/student/kvpn/vpn_cert/ipsec.secrets.a /etc/ipsec.secrets
 
 
-# nano ipsec conf and secrets
+sudo ipsec restart
+sudo ipsec statusall
+
+### RUN ###
+
+ping -c 4 192.168.70.6
 
 sudo ipsec statusall
 sudo ip xfrm state
 sudo ip xfrm policy
-
-ping -c 7 192.168.70.6
