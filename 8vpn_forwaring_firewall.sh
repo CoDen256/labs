83faaa5bea1b30d846cee931a043a9a2909a5739
Denys Chernyshov <denys.chernyshov@optimax-energy.de>
2022-12-30 21:34:47 +0100

    fix forwarding

--- 8vpn_forwaring_firewall.sh
+++ 8vpn_forwaring_firewall.sh
@@ -1,13 +1,40 @@
 #!/bin/bash
 
-# Server A + B
-#sudo ipsec statusall
-sudo ip xfrm state
-#sudo ip xfrm policy
+# Server A
+sudo cp /home/student/kvpn/vpn_forward_drop/sysctl.conf /etc/sysctl.conf
 
 # Client A
-ping 192.168.80.111 -c 4
-traceroute 192.168.80.111
+scp /home/student/kvpn/vpn_forward_drop/interfaces.ca student@192.168.60.111:/tmp/interfaces
+ssh -t student@192.168.60.111 "sudo mv /tmp/interfaces /etc/network/interfaces"
+
+
+# Server B
+scp /home/student/kvpn/vpn_forward_drop/sysctl.conf student@192.168.70.6:/tmp/sysctl.conf
+ssh -t student@192.168.70.6 "sudo mv /tmp/sysctl.conf /etc/sysctl.conf"
+
+# Client B
+
+# scp /home/student/kvpn/vpn_forward_drop/interfaces.cb student@192.168.80.111:/tmp/interfaces
+# ssh -t student@192.168.80.111 "sudo mv /tmp/interfaces /etc/network/interfaces"
+
+scp /home/student/kvpn/vpn_forward_drop/interfaces.cb student@192.168.70.6:/tmp/interfaces
+ssh -t student@192.168.70.6 "scp /tmp/interfaces student@192.168.80.111:/tmp/interfaces"
+ssh -t student@192.168.70.6 "ssh -t student@192.168.80.111 \"sudo mv /tmp/interfaces /etc/network/interfaces\""
+
+
+echo "Run on Client A: ping 192.168.80.111 -c 4 && traceroute 192.168.80.111"
+echo "Run on Client B: ping 192.168.60.111 -c 4 && traceroute 192.168.60.111"
+read T # a or b
+
+
+# Client A
+# ping 192.168.80.111 -c 4
+# traceroute 192.168.80.111
+
+# Server A + B
+sudo ipsec statusall
+sudo ip xfrm state
+sudo ip xfrm policy
 
 
 # FIREWALL
