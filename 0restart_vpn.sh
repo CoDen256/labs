eae17ad30978c2013ad1e5ee50be787023344ed2
Denys Chernyshov <denys.chernyshov@optimax-energy.de>
2023-01-02 11:34:45 +0100

    add revoking certificates

---
+++ 0restart_vpn.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp /home/student/kvpn/orig/ipsec.conf.a /etc/ipsec.conf
+cp /home/student/kvpn/orig/ipsec.secrets.a /etc/ipsec.secrets
+
+sudo ipsec restart
+
+cd /home/student/kasy22_ca
+openssl ca -config ca1/openssl.cnf -revoke ca1/certs/192.168.70.5.cert.pem
+openssl ca -config ca1/openssl.cnf -revoke ca1/certs/192.168.70.6.cert.pem
