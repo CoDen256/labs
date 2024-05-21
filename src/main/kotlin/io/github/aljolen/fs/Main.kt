28995d40b4e4d466e874c3c454c0f2ed3b26b4d2
aljolen <alina00427@gmail.com>
2024-05-21 15:45:12 +0200

    Add force unlink

--- src/main/kotlin/io/github/aljolen/fs/Main.kt
+++ src/main/kotlin/io/github/aljolen/fs/Main.kt
@@ -3,7 +3,7 @@ package io.github.aljolen.fs
 import io.github.aljolen.fs.console.Console
 
 fun main() {
-    val storage = MemoryStorage(16, 16)
+    val storage = MemoryStorage(32, 16)
     val io = FileIO(storage)
     val fs = DefaultFileSystem(io)
     val console = Console(fs, io, storage)
